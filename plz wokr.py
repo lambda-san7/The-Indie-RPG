fe8e565a5a6faa4e5c2b16a3d03f950befbd4007
lambda-san7 <codekiwi132@gmail.com>
2023-06-08 20:46:27 -0700

    i forgor

--- plz wokr.py
+++ plz wokr.py
@@ -1,45 +1,4 @@
-import pygame
-from assets import (
-    sprite,
-    window,
-    camera,
-)
-
-class tile:
-    def __init__(self,file):
-        self.sprite = sprite(file,w=50,h=50).sprite
-    def render(self, x, y):
-        window.blit(self.sprite,(x - camera.x, y - camera.y))
-
-class prop:
-    def __init__(self,file,size=()):
-        self.sprite = sprite(file,w=size[0],h=size[1]).sprite
-        self.h = size[1]
-    def render(self, x, y):
-        window.blit(self.sprite,(x - camera.x, y - camera.y))
-
-class none_holder:
-    h = 0
-    def render(x,y):
-        pass
-
-tiles = [
-    tile("road.gif"),
-    tile("sidewalk tiles.gif"),
-    tile("crosswalk_horizontal.gif"),
-    tile("grass.gif"),
-]
-
-props = [
-    none_holder,
-    prop("vending machine.gif",(52,80)),
-    prop("vending machine 2.gif",(52,80)),
-]
-
-location = None
-
-class tokyo:
-    loc_map = [[3 , 1 , 0 , 0 , 1 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , ],
+[[3 , 1 , 0 , 0 , 1 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , 3 , ],
 [1 , 1 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 0 , 1 , 1 , 1 , 1 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 3 , 3 , 3 , 3 , ],
 [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , ],
 [0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , ],
@@ -59,28 +18,3 @@ class tokyo:
 [1 , 1 , 0 , 0 , 1 , 3 , 1 , 0 , 0 , 1 , 1 , 1 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , 3 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 1 , 0 , 0 , 1 , 1 , 1 , 1 , 1 , ],
 [3 , 1 , 0 , 0 , 1 , 3 , 1 , 0 , 0 , 1 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 3 , 3 , 3 , 3 , 1 , 0 , 0 , 1 , 3 , 3 , 1 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , ],
 ]
-    prop_map = [
-        [0,0,0,0,1],
-        [0,0,0,0,2],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-        [0,0,0,0,0],
-    ]
-    def render(loc_map, prop_map):
-        x = 0
-        y = 0
-        for i in loc_map:
-            for o in loc_map[loc_map.index(i)]:
-                tiles[o].render(x,y)
-                x += 50
-            x = 0
-            y += 50
-
-        x = 0
-        y = 0
-        for i in prop_map:
-            for o in prop_map[prop_map.index(i)]:
-                props[o].render(x,(y - props[o].h) + 50)
-                x += 50
-            x = 0
-            y += 50
